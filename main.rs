a97a1f9629fec38fba155d592a81a132379f6370
soaresgabe <gabriel-soares@uergs.edu.br>
2024-04-23 18:39:56 -0300

    create vm

--- main.rs
+++ main.rs
@@ -3,14 +3,23 @@ mod machine;
 use machine::Machine;
 
 fn main() {
-    let mut mv = Machine::new(128);
+    let mut memory = vec![
+        200, // LA op (Load accumulator with value at memory location 0)
+        201, // LA op (Load accumulator with value at memory location 1)
+        202, // AA op (Add value at memory location 1 to accumulator)
+        210, // PW op (Print word at memory location 0)
+        211, // PW op (Print word at memory location 1)
+    ];
 
-    // Set up memory with instructions...
+    memory[0] = 10;
+    memory[1] = 20;
+
+    let mut vm = Machine::new(memory.len());
+    vm.memory.copy_from_slice(&memory);
 
     loop {
-        mv.execute_next_instruction();
-        if mv.pc >= mv.memory.len() {
-            eprintln!("Erro: Fim inesperado do programa.");
+        vm.execute_next_instruction();
+        if vm.pc >= vm.memory.len() {
             break;
         }
     }
